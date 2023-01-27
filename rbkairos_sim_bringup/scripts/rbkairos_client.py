2da0a674afb783526fa15ad76e8afc9fac3ea54f
Nuria Diaz <ndiaz@robotnik.es>
2023-01-27 12:01:14 +0100

    Update demo version

--- rbkairos_sim_bringup/scripts/rbkairos_client.py
+++ rbkairos_sim_bringup/scripts/rbkairos_client.py
@@ -6,6 +6,7 @@ import math
 import time
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 from control_msgs.msg import GripperCommandAction,GripperCommandGoal
+from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
 from tf.transformations import euler_from_quaternion, quaternion_from_euler
 
 
@@ -13,7 +14,9 @@ class RBKairosClient:
     def __init__(self):
         self.client = actionlib.SimpleActionClient('move_base',MoveBaseAction)
         self.client_gripper = actionlib.SimpleActionClient('gripper/gripper_controller/gripper_cmd',GripperCommandAction)
-        self.pos = rospy.get_param('~actions')
+        self.arm_pub = rospy.Publisher('arm/pos_traj_controller/command', JointTrajectory, queue_size=10)
+        self.actions = rospy.get_param('~actions')
+        self.arm_joints = rospy.get_param('~arm_joints')
         print("Waiting for action server to start.")
         self.client.wait_for_server()
         print("Action server started, sending goal.")
@@ -43,7 +46,7 @@ class RBKairosClient:
  
     def send_move_gripper(self, position, max_effort):
    
-        #crear mensaje move_gripper
+        # crear mensaje move_gripper
         goal = GripperCommandGoal()
 
         goal.command.position = position
@@ -51,7 +54,7 @@ class RBKairosClient:
 
         # enviar el goal a move_gripper
         self.client_gripper.send_goal(goal)
-        time.sleep(20)
+        time.sleep(15)
         wait = self.client_gripper.wait_for_result()
 
         if not wait:
@@ -59,6 +62,21 @@ class RBKairosClient:
             rospy.signal_shutdown("Action server gripper not available!")
         else:
             return self.client_gripper.get_result()
+
+    def send_move_arm(self, position):
+
+        goal = JointTrajectory()
+        p = JointTrajectoryPoint()
+        goal.header.stamp = rospy.Time.now()
+
+        p.positions = position
+        p.time_from_start = rospy.Duration.from_sec(4)
+        goal.joint_names = self.arm_joints   #se obtienen las articulaciones del brazo
+        goal.points.append(p)
+
+        self.arm_pub.publish(goal)
+        time.sleep(15)
+    
 
     def is_goto_valid(self, goto):
 
@@ -114,9 +132,28 @@ class RBKairosClient:
             rospy.logerr("The type of the variable 'max_effort' is not a float")
             return False
 
+    def is_movearm_valid(self, movearm):
+
+        if "positions" not in movearm:
+            rospy.logerr("Variable 'position' is missing")
+            return False
+
+        if len(movearm["positions"]) is not len(self.arm_joints):
+            rospy.logerr("The size of the varible 'position' is not the same as the number of joints")
+            return False
+    
+        if type(movearm["positions"]) is not list:
+            rospy.logerr("The type of the variable 'position' is not a float")
+            return False
+        
+        for i in movearm["positions"]:
+            if type(i) is not float:
+                rospy.logerr("The type of one of the list elements is not a float")
+                return False
+        
     def is_sequence_valid(self):
 
-        for action in self.pos:
+        for action in self.actions:
             if "type" not in action:
                 rospy.logerr("The type of action is not defined")
                 return False
@@ -128,7 +165,8 @@ class RBKairosClient:
                 action_valid = self.is_goto_valid(action)
             if action_type == "MOVEGRIPPER":
                 action_valid = self.is_movegripper_valid(action)
-
+            if action_type == "MOVEARM":
+                action_valid = self.is_movearm_valid(action)
 
             if action_valid == False:
                 return False
@@ -140,16 +178,21 @@ class RBKairosClient:
 
     def movegripper(self, movegripper):
         rbkairos_client.send_move_gripper(movegripper["position"],movegripper["max_effort"])
+    
+    def movearm(self, movearm):
+        rbkairos_client.send_move_arm(movearm["positions"])
          
     def execute_sequence(self):
 
-        for action in self.pos:
+        for action in self.actions:
             action_type = action["type"]
             rospy.loginfo("An action " + action_type + " will be executed.")
             if action_type == "GOTO":
                 action_valid = self.goto(action)    
             if action_type == "MOVEGRIPPER":
-                action_valid = self.movegripper(action)  
+                action_valid = self.movegripper(action)      
+            if action_type == "MOVEARM":
+                action_valid = self.movearm(action) 
    
 
 if __name__ == '__main__':
